081e8e78f64e495a1feee8ede8b832dd53b354b2
cwight2021 <86746422+cwight2021@users.noreply.github.com>
2022-05-31 10:58:40 -0600

    Update start-spark.sh

--- start-spark.sh
+++ start-spark.sh
@@ -4,21 +4,4 @@
 
 export SPARK_WORKER=`hostname -i`
 
-if [ "$SPARK_WORKLOAD" == "master" ];
-then
-
-export SPARK__HOST=`hostname`
-
-cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG
-
-elif [ "$SPARK_WORKLOAD" == "worker" ];
-then
-
-cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.worker.Worker --ip $SPARK_WORKER --port 9091  --webui-port 8080  spark://10.129.1.141:7077
-
-elif [ "$SPARK_WORKLOAD" == "submit" ];
-then
-    echo "SPARK SUBMIT"
-else
-    echo "Undefined Workload Type $SPARK_WORKLOAD, must specify: master, worker, submit"
-fi
+cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.worker.Worker --ip $SPARK_WORKER --port 9091 spark://spark-git-10-29c6q:7077
