284b5b1eab7ebdea860c2a4c8fb7a2da24d8806e
cwight2021 <86746422+cwight2021@users.noreply.github.com>
2022-05-31 09:30:24 -0600

    Update start-spark.sh

--- start-spark.sh
+++ start-spark.sh
@@ -6,8 +6,6 @@ if [ "$SPARK_WORKLOAD" == "master" ];
 then
 
 export SPARK_MASTER_HOST=`hostname`
-export worker1=9091
-export worker2=9092
 
 cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG
 
