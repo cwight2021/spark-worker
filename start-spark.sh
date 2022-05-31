66f9d501bd115af28e1e97e69728844948378085
cwight2021 <86746422+cwight2021@users.noreply.github.com>
2022-05-30 22:08:16 -0600

    Update start-spark.sh

--- start-spark.sh
+++ start-spark.sh
@@ -6,10 +6,12 @@ if [ "$SPARK_WORKLOAD" == "master" ];
 then
 
 export SPARK_MASTER_HOST=`hostname`
+export worker1=9091
+export worker2=9092
 
 cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG
-cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.worker.Worker --ip 10.128.0.1 --port 9091 -c 2 -m 2G spark://10.128.0.1:7077
-cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.worker.Worker --ip 10.128.0.1 --port 9092 -c 2 -m 2G spark://10.128.0.1:7077
+cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.worker.Worker --ip $SPARK_MASTER_HOST --port 9091 -c 2 -m 2G spark://10.128.0.1:7077
+cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.worker.Worker --ip $SPARK_MASTER_HOST --port 9092 -c 2 -m 2G spark://10.128.0.1:7077
 
 elif [ "$SPARK_WORKLOAD" == "worker" ];
 then
