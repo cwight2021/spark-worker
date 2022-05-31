f4de732d01325103ec053618eec5db209129917b
cwight2021 <86746422+cwight2021@users.noreply.github.com>
2022-05-31 09:10:21 -0600

    Update start-worker.sh

--- start-worker.sh
+++ start-worker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export SPARK_MASTER_HOST=`hostname-i`
+export SPARK_MASTER_HOST=`hostname -i`
 cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.worker.Worker --ip $SPARK_MASTER_HOST --port 9091 -c 2 -m 2G spark://$SPARK_MASTER_HOST:7077
