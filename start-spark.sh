465703f96038b4c8f72a05145f78455b40a05be4
cwight2021 <86746422+cwight2021@users.noreply.github.com>
2022-05-31 10:37:49 -0600

    Update start-spark.sh

--- start-spark.sh
+++ start-spark.sh
@@ -2,17 +2,19 @@
 
 . "/opt/spark/bin/load-spark-env.sh"
 
+export SPARK_WORKER=`hostname -i`
+
 if [ "$SPARK_WORKLOAD" == "master" ];
 then
 
-export SPARK_MASTER_HOST=`hostname`
+export SPARK__HOST=`hostname`
 
 cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.master.Master --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG
 
 elif [ "$SPARK_WORKLOAD" == "worker" ];
 then
 
-cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.worker.Worker --webui-port 8080  spark://10.128.0.144:7077
+cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.worker.Worker --ip $SPARK_WORKER --port 9091  --webui-port 8080  spark://10.128.0.144:7077
 
 elif [ "$SPARK_WORKLOAD" == "submit" ];
 then
