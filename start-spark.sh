4f7ce2d3f8503b155f81f7121d8324bb2d6d2c4e
cwight2021 <86746422+cwight2021@users.noreply.github.com>
2022-05-31 12:06:22 -0600

    Update start-spark.sh

--- start-spark.sh
+++ start-spark.sh
@@ -4,4 +4,4 @@
 
 export SPARK_WORKER=`hostname -i`
 
-cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.worker.Worker --ip $SPARK_WORKER --port 9091 spark://spark-git-10-29c6q:7077
+cd /opt/spark/bin && ./spark-class org.apache.spark.deploy.worker.Worker -c 3 -m 4G spark://spark-git-10-29c6q:7077
